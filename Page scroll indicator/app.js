c1a6d34333297c9e2f1852433de755bebbb10c96
anujbis2006 <anujbiswas303@gmail.com>
2025-02-03 01:14:16 +0530

    Add heart click animation

--- Page scroll indicator/app.js
+++ Page scroll indicator/app.js
@@ -1,10 +1,10 @@
 let indicator = document.querySelector(".scroll-indicator .progress");
-let scrollHeight = document.body.scrollHeight - document.documentElement.clientHeight;
+let scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
 
-window.addEventListener("scroll", scroll);
+window.addEventListener('scroll',scroll)
 
-function scroll() {
-  let scrollTop = document.documentElement.scrollTop;
-  let scrolled = (scrollTop / scrollHeight) * 100;
-  indicator.style.width = `${scrolled}%`;
+function scroll(){
+    let scrollTop = document.documentElement.scrollTop;
+    let srolled = (scrollTop / scrollHeight)*100;
+    indicator.style.width = `${scrolled}%`;
 }
